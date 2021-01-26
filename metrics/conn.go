6b5d419e87acaf4808b6eef2d7b3bea1df5c2ca1
Patrick Downey <patrickdowney@dioadconsulting.com>
2021-01-26 23:00:45 +0000

    Add metrics collector for net.Conn and net.Listener

---
+++ metrics/conn.go
@@ -0,0 +1,133 @@
+package metrics
+
+import (
+	"net"
+	"time"
+)
+
+type ReadWriteCounter interface {
+	BytesRead()
+	BytesWritten()
+}
+
+type ReadCounter interface {
+	BytesRead() int
+}
+
+type WrittenCounter interface {
+	BytesWritten() int
+}
+
+type ConnMetrics interface {
+	BytesRead() int
+	BytesWritten() int
+	ResetMetrics()
+	Duration() time.Duration
+	StartTime() time.Time
+	EndTime() time.Time
+}
+
+type connMetrics struct {
+	bytesRead    int
+	bytesWritten int
+
+	startTime time.Time
+	endTime   time.Time
+}
+
+type ListenerMetrics interface {
+	AcceptedCount() int
+	ResetMetrics()
+}
+
+type Conn struct {
+	conn    net.Conn
+	metrics *connMetrics
+}
+
+func (s *Conn) StartTime() time.Time {
+	return s.metrics.startTime
+}
+
+func (s *Conn) SetStartTime(t time.Time) {
+	s.metrics.startTime = t
+}
+
+func (s *Conn) EndTime() time.Time {
+	return s.metrics.endTime
+}
+
+func (s *Conn) SetEndTime(t time.Time) {
+	s.metrics.endTime = t
+}
+
+func (s *Conn) Duration() time.Duration {
+	if s.metrics.endTime.IsZero() {
+		return time.Since(s.metrics.startTime)
+	}
+
+	return s.metrics.endTime.Sub(s.metrics.startTime)
+}
+
+func (s *Conn) BytesRead() int {
+	return s.metrics.bytesRead
+}
+
+func (s *Conn) BytesWritten() int {
+	return s.metrics.bytesWritten
+}
+
+func (s *Conn) ResetMetrics() {
+	s.metrics = &connMetrics{}
+}
+
+func (s Conn) Read(b []byte) (int, error) {
+    n, err := s.conn.Read(b)
+	s.metrics.bytesRead += n
+	return n, err
+}
+
+func (s Conn) Write(b []byte) (int, error) {
+	n, err := s.conn.Write(b)
+	s.metrics.bytesWritten += n
+	return n, err
+}
+
+func (s Conn) Close() error {
+	s.SetEndTime(time.Now())
+	return s.conn.Close()
+}
+
+func (s Conn) LocalAddr() net.Addr {
+	return s.conn.LocalAddr()
+}
+
+func (s Conn) RemoteAddr() net.Addr {
+	return s.conn.RemoteAddr()
+}
+
+func (s Conn) SetDeadline(t time.Time) error {
+	return s.conn.SetDeadline(t)
+}
+
+func (s Conn) SetReadDeadline(t time.Time) error {
+	return s.conn.SetReadDeadline(t)
+}
+
+func (s Conn) SetWriteDeadline(t time.Time) error {
+	return s.conn.SetWriteDeadline(t)
+}
+
+func NewConn(c net.Conn) *Conn {
+	return NewConnWithStartTime(c, time.Now())
+}
+
+func NewConnWithStartTime(c net.Conn, startTime time.Time) *Conn {
+	return &Conn{
+		conn: c,
+		metrics: &connMetrics{
+			startTime: startTime,
+		},
+	}
+}
+
---
+++ metrics/listener.go
@@ -0,0 +1,45 @@
+package metrics
+
+import (
+	"net"
+)
+
+type Listener struct {
+	ln            net.Listener
+	acceptedCount int
+}
+
+func (l *Listener) ResetMetrics() {
+	l.acceptedCount = 0
+}
+
+func (l *Listener) AcceptedCount() int {
+	return l.acceptedCount
+}
+
+func (l *Listener) Accept() (*Conn, error) {
+	conn, err := l.ln.Accept()
+
+	if err != nil {
+		return nil, err
+	}
+
+	l.acceptedCount += 1
+	connWithMetrics := NewConn(conn)
+
+	return connWithMetrics, err
+}
+
+func (l *Listener) Close() error {
+	return l.ln.Close()
+}
+
+func (l *Listener) Addr() net.Addr {
+	return l.ln.Addr()
+}
+
+func NewListener(l net.Listener) *Listener {
+	return &Listener{
+		ln: l,
+	}
+}
