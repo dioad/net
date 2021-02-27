3e53246952fbca1be62adedcea988a4dc45500c2
Patrick Downey <patrickdowney@dioadconsulting.com>
2021-02-27 21:42:26 +0000

    Add pprof.Resource

---
+++ http/pprof/resource.go
@@ -0,0 +1,40 @@
+package pprof
+
+import (
+	"net/http/pprof"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/zerolog"
+)
+
+type Resource struct {
+	Logger zerolog.Logger
+}
+
+type Status struct {
+	Status string
+}
+
+// RegisterRoutes ...
+func (dr *Resource) RegisterRoutes(parentRouter *mux.Router) {
+parentRouter.HandleFunc("/", pprof.Index)
+	parentRouter.HandleFunc("/cmdline", pprof.Cmdline)
+	parentRouter.HandleFunc("/profile", pprof.Profile)
+	parentRouter.HandleFunc("/symbol", pprof.Symbol)
+
+	// Manually add support for paths linked to by index page at /debug/pprof/
+	parentRouter.Handle("/goroutine", pprof.Handler("goroutine"))
+	parentRouter.Handle("/heap", pprof.Handler("heap"))
+	parentRouter.Handle("/threadcreate", pprof.Handler("threadcreate"))
+	parentRouter.Handle("/block", pprof.Handler("block"))
+}
+
+func (dr *Resource) Status() (interface{}, error) {
+	return Status{Status: "OK"}, nil
+}
+
+func NewResource(logger zerolog.Logger) *Resource {
+	return &Resource{
+		Logger: logger,
+	}
+}
