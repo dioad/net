24350ede269bfc987cf3e45378ddf9a94e9b31f0
Patrick Downey <patrick.downey@dioadconsulting.com>
2021-11-26 22:41:09 +0000

    avoid runtime error if no access token is found

--- http/auth/github/client.go
+++ http/auth/github/client.go
@@ -16,10 +16,10 @@ func (a GitHubClientAuth) AddAuth(req *http.Request) error {
 	if a.accessToken == "" {
 		var err error
 		a.accessToken, err = resolveAccessToken(a.Config)
-		log.Debug().Str("accessTokenPrefix", a.accessToken[0:8]).Msg("readAccessToken")
 		if err != nil {
 			return err
 		}
+		log.Debug().Str("accessTokenPrefix", a.accessToken[0:8]).Msg("readAccessToken")
 	}
 
 	req.Header.Add("Authorization", fmt.Sprintf("bearer %v", a.accessToken))
