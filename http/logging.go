457852fb149643e714cdadc65b0f5bb79378404c
Pat Downey <patrick.downey@dioadconsulting.com>
2024-04-11 00:03:22 +0100

    feat: make http.Server logging more configurable

--- http/logging.go
+++ http/logging.go
@@ -18,8 +18,10 @@ func DefaultCombinedLogHandler(logWriter io.Writer) HandlerWrapper {
 	}
 }
 
-func structuredLogger(r *http.Request, status, size int, duration time.Duration) {
-	hlog.FromRequest(r).Info().
+type StructuredLoggerFormatter func(r *http.Request, status, size int, duration time.Duration) *zerolog.Logger
+
+func StandardLogger(r *http.Request, status, size int, duration time.Duration) *zerolog.Logger {
+	logger := hlog.FromRequest(r).With().
 		Str("method", r.Method).
 		Stringer("url", r.URL).
 		Int("status", status).
@@ -29,13 +31,28 @@ func structuredLogger(r *http.Request, status, size int, duration time.Duration)
 		Str("referer", r.Referer()).
 		Str("ip", r.RemoteAddr).
 		Str("proto", r.Proto).
-		Str("host", r.Host).
-		Msg("accessLog")
+		Str("host", r.Host).Logger()
+
+	return &logger
 }
 
+// func CoreDNSStandardLogger
+
+//func structuredLogger(r *http.Request, status, size int, duration time.Duration) {
+//	StandardLogger(r, status, size, duration).Info().Msg("accessLog")
+//}
+
 func ZerologStructuredLogHandler(logger zerolog.Logger) HandlerWrapper {
+	return ZerologStructuredLogHandlerWithFormatter(logger, StandardLogger)
+}
+
+func ZerologStructuredLogHandlerWithFormatter(logger zerolog.Logger, formatter StructuredLoggerFormatter) HandlerWrapper {
 	logReq := hlog.NewHandler(logger)
 	return func(next http.Handler) http.Handler {
+		structuredLogger := func(r *http.Request, status, size int, duration time.Duration) {
+			formatter(r, status, size, duration).Info().Msg("accessLog")
+		}
+
 		return logReq(hlog.AccessHandler(structuredLogger)(next))
 	}
 }
